8b897ee6598e00d3c7fc4ff163bb6848bdf61bbb
Razz67 <srasberry00@gmail.com>
2022-11-07 11:04:18 -0500

    Connect schema to routes

--- backend/routes/workoutRouter.js
+++ backend/routes/workoutRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const Workout = require('../models/workoutModel');
 
 const router = express.Router();
 
-
+// connect model data(workoutModel.js) to the routes
 // Get all workouts
 router.get('/', (req, res) => {
     res.json({msg: 'GET All Workouts'});
@@ -14,9 +15,22 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a workout 
-router.post('/', (req, res) => {
-    res.json({msg: 'POST a Workout'});
+router.post('/', async (req, res) => {
+    const {title, reps, load} = req.body;
+    try {
+        const workout = await Workout.create({
+            title,
+            reps,
+            load
+        })
+        res.status(200).json(workout);
+    }
+ catch (error) { 
+    res.status(400).json({error: error.message});
+} 
 });
+
+
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
