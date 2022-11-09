203fbf1e94ea6d9fefb67f36798f4af6d2077003
Razz67 <srasberry00@gmail.com>
2022-11-09 09:03:58 -0500

    Add Delete functionality

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,26 +1,32 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
-const WorkouDetails = ({ workout }) => {
+export const WorkoutDetails = ({ workout }) => {
+	const { dispatch } = useWorkoutsContext();
+
+	const handleClick = async () => {
+		const response = await fetch("http://localhost:5000/api/workouts/" + workout._id, {
+			method: "DELETE",
+		});
+		const json = await response.json();
+
+		if (response.ok) {
+			dispatch({ type: "DELETE_WORKOUT", payload: json });
+		}
+	};
+
 	return (
-		<div>
-			<div className=" col-sm-6">
-				<div className="card workout-details " style={{ width: "25rem" }}>
-					<div className="card-body">
-						<h4 className="card-title">{workout.title}</h4>
-						<p className="card-text">
-							<strong>Load: </strong>
-							{workout.load}(lbs)
-						</p>
-
-						<p className="card-text">
-							<strong>Reps: </strong>
-							{workout.reps}
-						</p>
-						<p>{workout.createdAt}</p>
-					</div>
-				</div>
-			</div>
+		<div className="workout-details">
+			<h4>{workout.title}</h4>
+			<p>
+				<strong>Load: </strong>
+				{workout.load}(lbs)
+			</p>
+			<p>
+				<strong>Reps: </strong>
+				{workout.reps}
+			</p>
+			<p>{workout.createdAt}</p>
+			<span onClick={handleClick}>delete</span>
 		</div>
 	);
 };
-
-export default WorkouDetails;
